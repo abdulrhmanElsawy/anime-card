7aa3e9fc6c9c521d8ecdc0e4920955ca6c9d16ca
abdulrhmanElsawy <your.email@example.com>
2024-08-08 13:14:14 +0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
+const isProd = process.env.NODE_ENV === 'production';
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: isProd ? '/abdelrhman-elsawy' : '',
+    assetPrefix: isProd ? '/abdelrhman-elsawy' : '',
+    
   images: {
     remotePatterns: [
       {
@@ -8,6 +14,18 @@ const nextConfig = {
       },
     ],
   },
+    
+    output: "export",
+    trailingSlash: true,
+    skipTrailingSlashRedirect: true,
+    reactStrictMode: true,
+    webpack(config, options) {
+        config.module.rules.push({
+            test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)$/,
+            use: 'file-loader',
+        });
+
+        return config;
+    },
 };
 
-module.exports = nextConfig;
